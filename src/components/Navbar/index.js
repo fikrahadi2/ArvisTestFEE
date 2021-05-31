7fffb0ce23b856aba907e2aaf035feb99f9bd187
fikrahadi <13518036@std.stei.itb.ac.id>
2021-06-01 05:48:30 +0700

    edit navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink } from './NavbarElements';
 import {FaBars } from 'react-icons/fa';
-import { Link } from 'react-router';
+import {Link} from 'react-router-dom';
 
 const Navbar = ({ toggle }) => {
     return (
@@ -19,19 +19,29 @@ const Navbar = ({ toggle }) => {
                             <NavLinks to="/">BERANDA</NavLinks>
                         </NavItem>
                         <NavItem>
-                            <NavLinks href="/pages/list">LIST BARANG</NavLinks>
+                            <NavLinks href="../../pages/ListBarang/">LIST BARANG</NavLinks>
                         </NavItem>
                         <NavItem>
                             <NavLinks to="cart">KERANJANG</NavLinks>
                         </NavItem>
                     </NavMenu>
                     <NavBtn>
-                        <NavBtnLink to="/login">LOGIN</NavBtnLink>
+                        <NavLink to="/login">LOGIN</NavLink>
                     </NavBtn>
                 </NavbarContainer>
             </Nav>
         </>
     )
 }
+
+function NavLink({ to, children }: NavLinkProp) {
+    return (
+      <NavBtnLink>
+        <Link href={to} passHref>
+          {children}
+        </Link>
+      </NavBtnLink>
+    );
+  }
 
 export default Navbar;
