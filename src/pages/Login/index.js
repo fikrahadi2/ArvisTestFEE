9b6be88b7d828ed1217d4b0a879f93236535b500
fikrahadi <13518036@std.stei.itb.ac.id>
2021-06-01 01:31:06 +0700

    login page but still have bug

---
+++ src/pages/Login/index.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react'
+import Navbar from '../../components/Navbar'
+import LoginLayout from '../../components/Login'
+import Footer from '../../components/Footer'
+import Sidebar from '../../components/Sidebar'
+
+const Login = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
+
+    return (
+        <>
+            <Sidebar isOpen={isOpen} toggle={toggle}/>
+            <Navbar toggle={toggle}/>
+            <LoginLayout />
+            <Footer/>
+        </>
+    )
+}
+
+export default Login
