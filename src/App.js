7884c915f104ba2726a5a7eddd02c2564703d8c3
fikrahadi <13518036@std.stei.itb.ac.id>
2021-06-01 01:33:41 +0700

    edit many file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router} from 'react-router-dom'; 
-import Home from './pages';
+import List from './pages/ListBarang';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
     <Router>
-      <Home />
+      <List />
     </Router>
   );
 }
---
+++ src/pages/ListBarang/index.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react'
+import ListBarangSection from '../../components/ListBarang'
+import Navbar from '../../components/Navbar'
+import Footer from '../../components/Footer'
+import Sidebar from '../../components/Sidebar'
+
+const ListBarang = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
+    return (
+        <>
+            <Sidebar isOpen={isOpen} toggle={toggle}/>
+            <Navbar toggle={toggle}/>
+            <ListBarangSection />
+            <Footer/>
+        </>
+    )
+}
+
+export default ListBarang
