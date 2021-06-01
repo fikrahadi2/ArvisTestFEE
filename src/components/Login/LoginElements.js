224058f24727864b4dacf3c7f4dac9250ebb8722
fikrahadi <13518036@std.stei.itb.ac.id>
2021-06-01 07:11:21 +0700

    edit login page

--- src/components/Login/LoginElements.js
+++ src/components/Login/LoginElements.js
@@ -5,6 +5,7 @@ export const LoginContainer = styled.div`
     background: #2AD576;
     display: flex;
     justify-content: center;
+    flex-direction: column;
     align-items: center;
     height: 450px;
     position: relative;
@@ -23,16 +24,6 @@ export const LoginContainer = styled.div`
     }
 `
 
-export const HomeContent = styled.div`
-    z-index: 3;
-    max-width: 1350px;
-    position: absolute;
-    padding 8px 24px;
-    display: flex;
-    flex-direction: column;
-    align-item: center;
-`
-
 export const LoginH1 = styled.h1`
     color: #000000;
     font-size: 48px;
@@ -44,5 +35,126 @@ export const LoginH1 = styled.h1`
 
     @media screen and (max-width: 480px) {
         font-size: 32px;
+    }
+`
+
+export const LoginItem = styled.div`
+    margin: 30px;
+    background: #2AD576;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    z-index: 1;
+    flex-direction: row;
+
+    :before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0:
+        bottom: 0;
+        background: linear-gradient(180deg, rgba(0,0,0,0.2) 0%, rgba(0,0,0,0.6) 100%),
+        linear-gradient(180deg, rgba(0,0,0,0.2) 0%, transparent 100%);
+        z-index: 2;
+    }
+`
+
+export const LoginUsername = styled.div`
+    margin: 5px;
+    background: #fff;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    border-radius: 10px;
+    max-height: 340px;
+    padding: 30px;
+    box-shadow: 0 1px 3 px rgba(0,0,0,0.2);
+    transitions: all 0.2s ease-in-out;
+    flex-wrap: wrap;
+    
+
+    &:hover {
+        transform: scale(1.02);
+        transitions: all 0.2s ease-in-out;
+        cursor: pointer;
+    }
+`
+
+export const LoginPassword = styled.div`
+    margin: 5px;
+    background: #fff;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    border-radius: 10px;
+    max-height: 340px;
+    padding: 30px;
+    box-shadow: 0 1px 3 px rgba(0,0,0,0.2);
+    transitions: all 0.2s ease-in-out;
+    flex-wrap: wrap;
+    
+
+    &:hover {
+        transform: scale(1.02);
+        transitions: all 0.2s ease-in-out;
+        cursor: pointer;
+    }
+`
+
+export const UsernameLabel = styled.p`
+    font-size: 1rem;
+    margin: 10px;
+    text-align: center;
+`
+
+export const PasswordLabel = styled.p`
+    font-size: 1rem;
+    margin: 10px;
+    text-align: center;
+`
+
+export const UsernameInput = styled.input`
+    font-size: 1rem;
+    margin: 10px;
+    text-align: center;
+`
+
+export const PasswordInput = styled.input`
+    font-size: 1rem;
+    margin: 10px;
+    text-align: center;
+`
+
+export const LoginBtn = styled.nav`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    @media screen and (max-width: 768px) {
+        display: none;
+    }
+`
+
+export const LoginBtnLink = styled(LinkR)`
+    border-radius: 10px;
+    background: #d8d8d8;
+    white-space: nowrap;
+    padding: 10px 22px;
+    color: #010606;
+    font-size: 16px;
+    outline: none;
+    border: none;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+    text-decoration: none;
+
+    &:hover {
+        transition: all 0.2s ease-in-out;
+        background: #6AE2A0;
+        color: #010606;
     }
 `
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,80 +1,25 @@
-import ClosableAlert from "./ClosableAlert";
-import StatefulInput from "./StatefulInput";
-//import { useAuth } from ".AuthContext";
-import router from "next/router";
-import React, {useState } from "react";
-import { Button, Form } from "react-bootstrap";
-import {LoginContainer, LoginH1,} from './LoginElements';
-import {Database} from './Database';
+import React/*, {useState}*/ from 'react';
+import {LoginContainer, LoginH1, LoginItem, LoginUsername, LoginPassword, UsernameInput, UsernameLabel, PasswordInput, PasswordLabel, LoginBtn, LoginBtnLink} from './LoginElements';
 
-function LoginLayout() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [loginState, setLoginState] = useState<RequestState>("none");
-  const auth = useAuth();
-
-  async function login() {
-    e.preventDefault();
-    try {
-      setLoginState("loading");
-      await auth.signin(email, password);
-      setLoginState("success");
-      router.push("/redirect");
-    } catch (err) {
-      setLoginState("error");
-    }
-  }
-
-  function showError() {
-    alert("Login gagal, periksa kembali email atau password");
-    setLoginState("none");
-  }
-
-  return (
-    <LoginContainer id="home">
-        <LoginH1>Login</LoginH1>
-        <Form className={styles.form} onSubmit={login}>
-            {loginState == "loading" ? (
-                <p className="text-danger">Loading...</p>
-            ) : loginState == "success" ? (
-            <ClosableAlert
-                message="Telah berhasil login"
-                title="Login Sukses!"
-                variant="success"
-            />
-            ) : (
-                loginState == "error" && showError()
-            )}
-            <StatefulInput
-                type="email"
-                inputClass={styles.input}
-                name="Email"
-                state={email}
-                setState={setEmail}
-                icon={<i className={`fas fa-envelope ${styles.icon}`}></i>}
-            />
-            <StatefulInput
-                type="password"
-                inputClass={styles.input}
-                name="Password"
-                state={password}
-                setState={setPassword}
-                icon={<i className={`fas fa-key ${styles.icon}`}></i>}
-            />
-            <Button variant="primary" type="submit" className={styles.submit}>
-                Submit
-            </Button>
-            <Form.Group controlId="question" className={styles.question}>
-            <Form.Label>
-                Belum punya akun?{" "}
-                <a href="/signup">
-                <b>Daftar</b>
-                </a>
-            </Form.Label>
-            </Form.Group>
-        </Form>
-    </LoginContainer>
-    );
+const LoginSection = () => {
+    return (
+        <LoginContainer id="home">
+            <LoginH1>Login</LoginH1>
+            <LoginItem>
+                <LoginUsername>
+                    <UsernameLabel>Username</UsernameLabel>
+                    <UsernameInput type="text"></UsernameInput>
+                </LoginUsername>
+                <LoginPassword>
+                    <PasswordLabel>Password</PasswordLabel>
+                    <PasswordInput type="text"></PasswordInput>
+                </LoginPassword>
+            </LoginItem>
+            <LoginBtn>
+                <LoginBtnLink to="/buy">SUBMIT</LoginBtnLink>
+            </LoginBtn>
+        </LoginContainer>
+    )
 }
 
-export default LoginLayout;
+export default LoginSection
